e845267fef3cde08b2f431b7a6fab8e259758423
Avnish Choubey <118668098+AvnishChoubey@users.noreply.github.com>
2025-10-06 18:39:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
+	//write your code here
+	const heap = new MinPriorityQueue();
+    arr.forEach(length => heap.enqueue(length));
   
+    let totalCost = 0;
+  
+    while (heap.size() > 1) 
+	{
+        const rope1 = heap.dequeue().element;
+        const rope2 = heap.dequeue().element;
+      
+        const cost = rope1 + rope2;
+        totalCost += cost;
+		
+        heap.enqueue(cost);
+    }
+
+    return totalCost;
 }
 
 module.exports=mincost;
